2fa20cb627b087ee88a5d4905e236d31c112a2f1
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-09 12:45:32 -0500

    Update 2_Values.py

--- pages/2_Values.py
+++ pages/2_Values.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
